49a69be94c0e7539e82081498afb6134e2df66b2
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2015-05-24 16:52:51 +0900

    gulp & scss実装基盤追加

---
+++ fuel/app/assets/gulpfile.js
@@ -0,0 +1,41 @@
+/* laod modules */
+var gulp      = require('gulp');
+var sass      = require('gulp-sass');
+var plumber   = require('gulp-plumber');
+var uglify    = require('gulp-uglify');
+var concat    = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+
+
+/* define path */
+var scssInputPath  = './scss/scss_src/*.scss';
+var scssOutputPath = './scss/css_src';
+var cssInputPath   = './scss/css_src/*.css';
+var cssOutputPath  = '../../../public/assets/css';
+
+
+/* define tasks */
+// scss -> css
+gulp.task('sass', function(){
+  gulp.src(scssInputPath)
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest(scssOutputPath));
+});
+
+// css -> concat -> minify
+gulp.task('css', function(){
+  var minifiedFileName = 'rootkey.min.css';
+  gulp.src(cssInputPath)
+    .pipe(concat(minifiedFileName))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(cssOutputPath));
+})
+
+// watch for scss compile
+gulp.task('watch-scss', function(){
+  gulp.watch(scssInputPath, ['sass', 'css']);
+})
+
+gulp.task('watch', ['watch_scss']);
+gulp.task('default', ['sass', 'css']);
