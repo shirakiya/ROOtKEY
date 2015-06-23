74ff346d838f9a46f4da987b9334a7a774b303db
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2015-06-24 00:04:23 +0900

    Marionette.js導入途中

--- fuel/app/assets/js/main.js
+++ fuel/app/assets/js/main.js
@@ -1,5 +1,25 @@
-var browserify = require('browserify');
+'use strict';
 
-var foo = require('./foo.js');
-var x = foo(100);
-console.log(x);
+var $ = require('jquery');
+var jQuery = $;
+var foundation = require('foundation-sites');
+var _ = require('underscore');
+var Backbone = require('backbone');
+Backbone.$ = $;
+var Marionette = require('backbone.marionette');
+
+var app = new Marionette.Application();
+app.addRegions({
+  body: 'body',
+  header: '#header',
+  content: '#content',
+  footer: '#footer'
+});
+
+app.addInitializer(function(){
+  $(document).foundation();
+  console.log('hoge');
+  console.log(app.body);
+});
+
+app.start();
--- fuel/app/views/element/footer.php
+++ fuel/app/views/element/footer.php
@@ -1,9 +1,11 @@
-<hr>
-<div class="row">
-	<div class="large-12 columns">
-		<div style="text-align: right;">
-			<p><a href="<?php echo Uri::create('help'); ?>"><i class="fa fa-question-circle"></i> ヘルプ</a></p>
-			<p>ROOtKEY created by shirakiya</p>
+<div id="footer">
+	<hr>
+	<div class="row">
+		<div class="large-12 columns">
+			<div style="text-align: right;">
+				<p><a href="<?php echo Uri::create('help'); ?>"><i class="fa fa-question-circle"></i> ヘルプ</a></p>
+				<p>ROOtKEY created by shirakiya</p>
+			</div>
 		</div>
 	</div>
 </div>
